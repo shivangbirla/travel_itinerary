4a222347d145ef10e35ccad007d2cd4def689f9e
shivangbirla <birlashivang01@gmail.com>
2024-01-17 23:38:13 +0530

    login-data api

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,31 +1,68 @@
 import React from "react";
 import Logo from "../assets/logo.jpg";
-// import { useAuth0 } from "@auth0/auth0-react";
+import { useAuth0 } from "@auth0/auth0-react";
+import { useEffect } from "react";
 
 const Navbar = () => {
-  // const { loginWithRedirect, isAuthenticated, logout } = useAuth0();
+  const { loginWithRedirect, isAuthenticated, logout, user } = useAuth0();
+  console.log(user);
+  useEffect(() => {
+    if (isAuthenticated) {
+      sendUserData();
+    }
+  }, [isAuthenticated, user]);
+
+  const sendUserData = async () => {
+    try {
+      const response = await fetch("http://localhost:8000/user_data", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          _id: user?.user_id,
+          name: user?.username,
+          email: user?.email,
+        }),
+      });
+
+      if (response.ok) {
+        console.log("User data sent successfully!");
+      } else {
+        console.error("Failed to send user data");
+      }
+    } catch (error) {
+      console.error("Error sending user data:", error);
+    }
+  };
+
   return (
     <nav className="fixed top-0 w-full flex justify-between px-5 md:px-20 py-3  items-center z-50  shadow-md shadow-gray-950 backnavdrop bg-[#1A3129]">
       <div className="flex items-center gap-4 text-white font-semibold text-lg ">
         <img src={Logo} alt="logo" className="w-10 rounded-full" />
         <h1 className="text-white hover:text-gray-300">Hypothetica</h1>
       </div>
-      {/* {isAuthenticated ? (
-        <button onClick={() => logout()} className="text-white">
-          Logout
-        </button>
-      ) : (
-        <button onClick={() => loginWithRedirect()} className="text-white">
-          Log In
-        </button>
-      )} */}
-      {/* <div className="flex  justify-between items-center gap-4 w-full">
-          
-        </div> */}
-      <div>
-        <button className="bg-[#CBEA7B] px-[16px] py-[12px] rounded-lg text-[14px] text-[#262626] font-semibold">
+
+      {/* <div className="flex  justify-between items-center gap-4 w-full"></div> */}
+      <div className="">
+        <button className="bg-[#CBEA7B] px-[16px] py-[12px] rounded-lg text-[14px] text-[#262626] font-semibold mr-[10px]">
           Contact Us
         </button>
+        {isAuthenticated ? (
+          <button
+            onClick={() => logout()}
+            className="bg-[#CBEA7B] px-[16px] py-[12px] rounded-lg text-[14px] text-[#262626] font-semibold"
+          >
+            Logout
+          </button>
+        ) : (
+          <button
+            onClick={() => loginWithRedirect()}
+            className="bg-[#CBEA7B] px-[16px] py-[12px] rounded-lg text-[14px] text-[#262626] font-semibold"
+          >
+            Log In
+          </button>
+        )}
       </div>
     </nav>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-// import { Auth0Provider } from "@auth0/auth0-react";
+import { Auth0Provider } from "@auth0/auth0-react";
 import "./index.css";
 import { NextUIProvider } from "@nextui-org/react";
 import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    
+    <Auth0Provider
+      domain="dev-gk0hpu0oz2poqc8r.us.auth0.com"
+      clientId="EqMTnG2islGuO9sDsW0a3q2oZZfVuj53"
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}
+    >
       <Router>
         <NextUIProvider>
           <App />
         </NextUIProvider>
       </Router>
+    </Auth0Provider>
   </React.StrictMode>
 );
