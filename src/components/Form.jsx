f11fb2ffd627f19458a13df51adb343510cb7021
Priyansh <priyanshgoel05@gmail.com>
2024-01-26 22:56:58 +0530

    Update URL

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -54,7 +54,7 @@ const Form = () => {
 
   const basicOutputRef = useRef(null);
 
-  const BASE_URL = import.meta.env.VITE_SERVER_URI;
+  const BASE_URL = "https://api.nutrichimp.zencoresolutions.co/";
 
   //  const BASE_URL = "https://generative-travel-itinerary.vercel.app";
 
