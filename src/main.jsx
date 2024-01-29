1cc3aad248045543ae8b2c7a3d9d5f6e3aaf4219
gitatractivo <gitanshutalwar@gmail.com>
2024-01-30 03:38:37 +0530

    clerk added

--- src/main.jsx
+++ src/main.jsx
@@ -5,22 +5,20 @@ import { Auth0Provider } from "@auth0/auth0-react";
 import "./index.css";
 import { NextUIProvider } from "@nextui-org/react";
 import { BrowserRouter as Router } from "react-router-dom";
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+const PUBLISHABLE_KEY =
+  "pk_test_Y3J1Y2lhbC1yaW5ndGFpbC0xMi5jbGVyay5hY2NvdW50cy5kZXYk";
 import { ClerkProvider } from "@clerk/clerk-react";
-
 
 if (!PUBLISHABLE_KEY) {
   throw new Error("Missing Publishable Key");
 }
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  
-    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-      <Router>
-        <NextUIProvider>
-          <App />
-        </NextUIProvider>
-      </Router>
-    </ClerkProvider>
-  
+  <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+    <Router>
+      <NextUIProvider>
+        <App />
+      </NextUIProvider>
+    </Router>
+  </ClerkProvider>
 );
